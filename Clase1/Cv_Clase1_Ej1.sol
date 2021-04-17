8176e4b3c894ae25ef742b0bbc94b9c64e608266
Rafael Ibarra <rafael.dante@gmail.com>
2021-04-16 22:15:31 -0300

    clase 1

---
+++ Clase1/Cv_Clase1_Ej1.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+contract Cv_Clase1_Ej1 {
+    
+    string public mi_variable;
+    
+    function Cv_Clase1_Ej1 () {
+        mi_variable = "Agun dato";
+    }
+    
+    // la ejecucion del contrato insume 10 unidades de gas
+    
+    
+    
+    // cada unidades de gas tiene un costo en ether
+}
